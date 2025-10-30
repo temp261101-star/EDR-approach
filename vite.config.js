c78a7ec88bbad32a63a4bef73fb57fe15a85cf0b
gayathri <gayathrivelox@gmail.com>
2025-10-30 16:42:05 +0530

    Api testing with proxy

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,32 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+//     base: '/',  
+//   server: {
+//     historyApiFallback: true,
+//     proxy: {
+       
+//       "/api/proxy": {
+//         target: "http://182.48.194.218:9191/api/v1",
+//         changeOrigin: true,
+      
+//         rewrite: (path) => path.replace(/^\/api\/proxy/, ""),
+//       },
+//     },
+//   },
+// });
+
+
+
+
+
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
